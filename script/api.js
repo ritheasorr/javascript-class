baaa3a22fef191a0e49ed98f430199b66d3ee375
ritheasor <ritheasorlol@gmail.com>
2025-10-06 16:33:21 +0700

    api https://imdb.iamidiotareyoutoo.com/search?q=

--- script/api.js
+++ script/api.js
@@ -1,6 +1,29 @@
-searchTitle = (title)=> {
+searchTitle = (title) => {
     fetch('https://imdb.iamidiotareyoutoo.com/search?q=' + encodeURIComponent(title))
         .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+        .then(data => {
+            const resultsDiv = document.getElementById('searchResults');
+            resultsDiv.innerHTML = '';
+
+            if (!data.description || data.description.length === 0) {
+                resultsDiv.innerHTML = '<p>No results found.</p>';
+                return;
+            }
+
+            data.description.forEach(movie => {
+                const movieDiv = document.createElement('div');
+                movieDiv.innerHTML = `
+                    <h3>${movie['#TITLE']} (${movie['#YEAR']})</h3>
+                    <img src="${movie['#IMG_POSTER']}" alt="${movie['#TITLE']}" width="120"><br>
+                    <strong>Actors:</strong> ${movie['#ACTORS']}<br>
+                    <a href="${movie['#IMDB_URL']}" target="_blank">IMDB Link</a>
+                    <hr>
+                `;
+                resultsDiv.appendChild(movieDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('searchResults').innerHTML = '<p>Error fetching results.</p>';
+        });
 }
